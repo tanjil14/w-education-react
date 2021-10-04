302b3553b0dd1495620af93b15b80df6303fbfd3
tanjil <tanjil14@cse.pstu.ac.bd>
2021-10-04 22:46:29 +0600

    readme updated

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+# W-Education
 
+[Quick visit](https://w-education.netlify.app/abou)
 
+## In this project:
+
+- use tailwind and custom css
+- use react router ,fontAwesome
+- responsive for all devices
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
     <div className="not-found flex items-center justify-center text-center p-42">
       <div className="content">
         <img className="border-none" src={img} alt="" />
-        <h5 className="text-red-600 font-bold text-4xl">
+        <h5 className="text-red-600 font-bold text-4xl mt-4">
           Opps! Page Not Found.
         </h5>
       </div>
